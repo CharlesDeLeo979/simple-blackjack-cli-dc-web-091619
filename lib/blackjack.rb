bbaf013841aa66a5b7e8fcec0df17a80102cd3c2
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-17 14:07:45 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -58,7 +58,7 @@ end
 def runner
   welcome 
   card_total = initial_round
-  binding.pry
+  # binding.pry
   until card_total > 21 
     card_total = hit?(card_total)
    display_card_total(card_total)
